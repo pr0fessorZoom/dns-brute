68fe0e72a206254e2a1eeb92901e969e01af75d4
r3verse <sebastian.ohberg@proton.me>
2024-09-09 09:37:01 -0300

    [2.0.0] Automatizacion de uso de diccionarios

---
+++ dicts/dicts.md
@@ -0,0 +1,9 @@
+# Dicts
+Aqui van los diccionarios para trabajar.
+
+## Recomendaciones
+Copiar los diccionarios a la carpeta `dicts` en el directorio de trabajo. Si no, hay que modificar las rutas de los diccionarios.
+```bash
+    cp /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt ./dicts
+    cp /usr/share/seclists/Discovery/Web-Content/raft-large-words-lowercase.txt ./dicts
+```
--- script.py
+++ script.py
@@ -6,7 +6,10 @@ if not os.path.exists('scripts'):
 
 # Solicitar al usuario el nombre del archivo .sh
 output_script = input("Escribe el nombre del archivo .sh (sin extensión): ") + ".sh"
-diccionario = "/usr/share/wordlists/" + input("Escribe el nombre del diccionario (sin extensión): ") + ".txt"
+top_1m = './dicts/subdomains-top1million-110000.txt'
+raft = './dicts/raft-large-words-lowercase-cleanned.txt' # Hay que poner la ruta del diccionario limpio
+letter = './dicts/letter.txt'
+diccionarios = [top_1m, raft, letter]
 
 # Ruta completa donde se guardará el archivo .sh
 output_path = os.path.join('scripts', output_script)
@@ -39,7 +42,12 @@ while True:
         resultado_dominio = f"{dominio_base}"
 
         # Generar el comando con los parámetros dados
-        comando = f"dnsrecon -d {dominio} -t brt -D {diccionario} -c {resultado_dominio}_result.csv"
+        for diccionario in diccionarios:
+            comando = f"dnsrecon -d {dominio} -t brt -D {diccionario} -c {resultado_dominio}_result.csv"
+            with open(output_path, 'a') as file:
+                file.write(comando + "\n")
+            print(f"Comando añadido: {comando}")
+
 
         # Guardar el comando en el archivo .sh
         with open(output_path, 'a') as file:
@@ -48,36 +56,38 @@ while True:
         print(f"Comando añadido: {comando}")
 
     elif accion == 'eliminar':
-        # Solicitar el nombre del dominio
-        dominio_base = input("Escribe el nombre del dominio a eliminar (sin '.com' o '.cl'): ")
+            # Solicitar el nombre del dominio
+            dominio_base = input("Escribe el nombre del dominio a eliminar (sin '.com' o '.cl'): ")
 
-        # Solicitar al usuario el tipo de dominio
-        tipo_dominio = input("¿Es un dominio .com o .cl? (escribe 'com' o 'cl'): ").lower()
+            # Solicitar al usuario el tipo de dominio
+            tipo_dominio = input("¿Es un dominio .com o .cl? (escribe 'com' o 'cl'): ").lower()
 
-        if tipo_dominio not in ['com', 'cl']:
-            print("Tipo de dominio no válido. Intenta nuevamente.")
-            continue
+            if tipo_dominio not in ['com', 'cl']:
+                print("Tipo de dominio no válido. Intenta nuevamente.")
+                continue
 
-        # Generar el dominio completo
-        dominio = f"{dominio_base}.{tipo_dominio}"
-        resultado_dominio = f"{dominio_base}"
+            # Generar el dominio completo
+            dominio = f"{dominio_base}.{tipo_dominio}"
+            resultado_dominio = f"{dominio_base}"
 
-        # Comando que se quiere eliminar
-        comando_a_eliminar = f"dnsrecon -d {dominio} -t brt -D {diccionario} -c {resultado_dominio}_result.csv"
+            # Leer el archivo y filtrar los comandos
+            with open(output_path, 'r') as file:
+                lineas = file.readlines()
 
-        # Leer el archivo y filtrar los comandos
-        with open(output_path, 'r') as file:
-            lineas = file.readlines()
+            # Filtrar las líneas que no coinciden con el comando a eliminar
+            with open(output_path, 'w') as file:
+                for linea in lineas:
+                    eliminar = False
+                    for diccionario in diccionarios:
+                        comando_a_eliminar = f"dnsrecon -d {dominio} -t brt -D {diccionario} -c {resultado_dominio}_result.csv"
+                        if linea.strip() == comando_a_eliminar:
+                            eliminar = True
+                            print(f"Comando eliminado: {comando_a_eliminar}")
+                            break
+                    if not eliminar:
+                        file.write(linea)
 
-        # Filtrar las líneas que no coinciden con el comando a eliminar
-        with open(output_path, 'w') as file:
-            for linea in lineas:
-                if linea.strip() != comando_a_eliminar:
-                    file.write(linea)
                 else:
-                    print(f"Comando eliminado: {comando_a_eliminar}")
-
-    else:
-        print("Acción no válida. Intenta nuevamente.")
+                    print("Acción no válida. Intenta nuevamente.")
 
 print("¡Operación finalizada!")
